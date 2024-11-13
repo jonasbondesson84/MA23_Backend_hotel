d22eb5843c551d3b5032d4397780f4c6ab60137d
Andreas Selguson <github@selguson.se>
2024-11-13 20:41:18 +0100

    fixes to post rooms & add rooms to db

---
+++ functions/rooms/index.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+
+    const rooms = [
+        "single","single","single","single","single","single","single",
+        "double","double","double","double","double","double","double",
+        "suite","suite","suite","suite","suite","suite"
+    ];
+
+    const roomPromises = rooms.map((room, index) => {
+        return db.put({
+            TableName: 'rooms-db',
+            Item: {
+                id: `room-${index + 1}`,
+                type: room
+            }
+        }).promise();
+    });
+
+    await Promise.all(roomPromises);
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Rooms added successfully' })
+    };
+};
